c8517f36b6e4b8e52928f0b8c168980a8ca4de92
Alex Aguirre <teamaguirre@gmail.com>
2022-08-06 11:36:39 -0700

    Added the Model Data

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// DEPENDENCIES
 const express = require('express')
 
 // CONFIGURATION
@@ -8,10 +7,15 @@ const app = express()
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
-})
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+  console.log('listening on port ',PORT);
 })
